ab911b7e41e5278f1e468a4cc84e65fca3ac0ccc
Seubpong Monsar <pjame.fb@gmail.com>
2021-06-18 22:06:46 +0700

    Set SA key

--- 01-setup/terraforms/service-account.tf
+++ 01-setup/terraforms/service-account.tf
@@ -7,3 +7,8 @@ resource "google_project_iam_member" "github-action-with-role" {
   role = "roles/owner"
   member = "serviceAccount:${google_service_account.github-action.email}"
 }
+
+resource "google_service_account_key" "github-action-key" {
+  service_account_id = google_service_account.github-action.name
+  public_key_type    = "TYPE_X509_PEM_FILE"
+}
