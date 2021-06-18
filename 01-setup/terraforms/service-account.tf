cf4fb326bc02d9664bb44c07bba4b429f6bc1fe3
Seubpong Monsar <pjame.fb@gmail.com>
2021-06-18 21:52:29 +0700

    Set IAM

--- 01-setup/terraforms/service-account.tf
+++ 01-setup/terraforms/service-account.tf
@@ -2,3 +2,8 @@ resource "google_service_account" "github-action" {
   account_id   = "github-action"
   display_name = "Github action service account"
 }
+
+resource "google_service_account_iam_binding" "github-action-iam" {
+  service_account_id = google_service_account.github-action.name
+  role               = "roles/owner"
+}
