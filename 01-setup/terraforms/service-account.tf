a3e8d20089e7519f54b05a67a7b033442e6ff1ff
Seubpong Monsar <pjame.fb@gmail.com>
2021-06-18 21:19:54 +0700

    Added service account

--- 01-setup/terraforms/service-account.tf
+++ 01-setup/terraforms/service-account.tf
@@ -1,4 +1,4 @@
-resource "google_service_account" "service_account" {
+resource "google_service_account" "github-action" {
   account_id   = "github-action"
   display_name = "Github action service account"
 }
